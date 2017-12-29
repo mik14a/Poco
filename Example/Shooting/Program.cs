bc8e0b843c11c50c3c462f47407f0dbcee3d38f9
mik14a <mik14a@gmail.com>
2017-12-29 17:37:34 +0900

    Add Examples

---
+++ Example/Shooting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shooting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
